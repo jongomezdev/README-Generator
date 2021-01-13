5a5055460a4b1cdf6dcd8e47ae3adcbbbb84017f
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-13 14:23:42 -0500

    testing output

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please type a brief description of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project?",
+    },
+  ]);
+
+const generateReadMe = (answers) =>
+  `
+  # ${answers.title}
+
+  ## ${answers.description}
+  ---
+  ## ${answers.installation}
+
+  `;
